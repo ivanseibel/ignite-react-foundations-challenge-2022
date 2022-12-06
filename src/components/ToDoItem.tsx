d9ab55e854d3b6c1c03f1ac020ed08337eb13cc1
Ivan Seibel <me@ivanseibel.dev>
2022-12-06 18:44:53 +0000

    integrate with useToDo hook

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -3,21 +3,20 @@ import * as Checkbox from '@radix-ui/react-checkbox';
 import { CheckIcon } from '@radix-ui/react-icons';
 
 import styles from './ToDoItem.module.css';
+import { ToDo } from '../hooks/useToDo';
 
-interface ToDoItemProps {
-  id: number;
-}
-
-export const ToDoItem = ({ id }: ToDoItemProps) => {
+export const ToDoItem = ({ id, description, isComplete }:ToDo) => {
   return (
     <div className={styles.toDoItem}>
-      <Checkbox.Root className="" id={`c1-${id}`}>
+      <Checkbox.Root 
+        id={`c1-${id}`}
+      >
         <Checkbox.Indicator className="CheckboxIndicator">
           <CheckIcon />
         </Checkbox.Indicator>
       </Checkbox.Root>
       <label htmlFor={`c1-${id}`}>
-        Sint adipisicing velit veniam pariatur aliquip duis. Pariatur in anim dolor dolor est labore deserunt nulla ex Lorem. Sit duis enim anim fugiat elit amet cillum deserunt id et sunt dolor.Reprehenderit Lorem officia nisi duis.
+        {description}
       </label>
       <button type="button" className={styles.deleteButton} >
         <Trash width={14} />
